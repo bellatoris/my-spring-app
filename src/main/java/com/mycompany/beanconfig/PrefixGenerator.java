0e521d521876e12125d956991d9088379309d7b8
Doogie <doogie.min@epapyrus.com>
2018-05-03 11:15:42 +0900

    [spring] beanconfig

---
+++ src/main/java/com/mycompany/beanconfig/PrefixGenerator.java
@@ -0,0 +1,5 @@
+package com.mycompany.beanconfig;
+
+public interface PrefixGenerator {
+	public String getPrefix();
+}
---
+++ src/main/java/com/mycompany/beanconfig/ReverseGenerator.java
@@ -0,0 +1,11 @@
+package com.mycompany.beanconfig;
+
+
+public class ReverseGenerator {
+
+	private int initial;
+
+	public void setInitial(int initial) {
+		this.initial = initial;
+	}
+}
